db896033e80edcb89f758c8ac344bc88ab5d76ed
vasiljevas <andrius.vasiljevas@nortal.com>
2023-10-01 18:38:07 +0300

    add deployment yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/linking-demo/",
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/linking-demo/";
+  }
+
+  return config;
+});
